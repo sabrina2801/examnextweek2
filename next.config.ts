9c9efad8226d875100e31e26f1a7a479062e412e
sabrina2801 <sa2211qq@gmail.com>
2025-10-19 01:56:04 +0500

    a

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,14 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 
-// Указываем точный путь к request.ts
-const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
-
 const nextConfig: NextConfig = {
-  // ваши опции Next.js
+    // ВАЖНОЕ ДОБАВЛЕНИЕ ДЛЯ ТЕКУЩИХ ВЕРСИЙ NEXT.JS С TURBOPACK
+    // Это предоставляет next-intl пространство для правильного разрешения алиасов
+    turbopack: {}, 
+    // ... ваши другие опции (если есть)
 };
+
+// Подключаем next-intl
+const withNextIntl = createNextIntlPlugin();
 
 export default withNextIntl(nextConfig);
