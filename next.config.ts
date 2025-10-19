60fc7cdca5fd30d049735c9b31b2ce0a09f5c931
sabrina2801 <sa2211qq@gmail.com>
2025-10-19 14:57:39 +0500

    c

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,11 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 
-// 💡 УКАЗЫВАЕМ ТОЧНЫЙ ПУТЬ
-const withNextIntl = createNextIntlPlugin('./src/app/i18n/request.ts');
-
 const nextConfig: NextConfig = {
-    // Не забудьте про turbopack, как было рекомендовано ранее!
-    // Это, вероятно, все еще необходимо для работы с Next.js 15.5.5 и Turbopack.
-    turbopack: {}, 
+  // ваши опции (если есть)
 };
+
+// Подключаем next-intl
+const withNextIntl = createNextIntlPlugin();
 
 export default withNextIntl(nextConfig);
